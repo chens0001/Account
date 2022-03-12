4887bc6682e0f6de2002753d48acb69b9bc886e4
mac <mac@macdeMacBook-Air.local>
2022-03-12 15:25:42 +0800

    完成icon 组件

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,12 +1,23 @@
 <template>
   <nav>
-    <router-link to="/money">记账</router-link>
-    <router-link to="/labels" >标签</router-link>
-    <router-link to="/statistics">统计</router-link>
+    <router-link to="/money">
+      <Icon name="money"/>
+      记账
+    </router-link>
+    <router-link to="/labels" >
+      <Icon name="label" />
+      标签
+    </router-link>
+    <router-link to="/statistics">
+      <Icon name="chart" />
+      统计
+    </router-link>
   </nav>
 </template>
 
 <script lang='ts'>
+import x from '@/assets/icons/label.svg';
+console.log(x)
 export default {
   name: 'Nav'
 };

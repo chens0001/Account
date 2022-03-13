7af64da4ec8a90c1e201be0e84c6911a369056cf
mac <mac@macdeMacBook-Air.local>
2022-03-13 20:43:33 +0800

    notes添加v-model

--- src/components/Money/Types.vue
+++ src/components/Money/Types.vue
@@ -19,16 +19,20 @@
 
 <script lang='ts'>
 import Vue from 'vue';
-import {Component} from 'vue-property-decorator';
+import {Component, Prop} from 'vue-property-decorator';
 
 @Component
 export default class Types extends Vue {
   type = '-'; // '-' 表示支出 '+'表示收入
+  @Prop() test: string| undefined;
   selectType(type: string) {
     if(type !== '-' && type !== '+') {
       throw new Error('type is unknown');
     }
     this.type = type;
+  }
+  mounted() {
+    console.log(this.test,'test')
   }
 }
 </script>
--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,7 +1,7 @@
 <template>
   <Layout class-prefix="layout">
     <NumberPad />
-    <Types/>
+    <Types test="test"/>
     <Notes/>
     <Tags/>
   </Layout>
